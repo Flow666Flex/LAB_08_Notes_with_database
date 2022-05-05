73d1694506c9dd49a6171a0b3d4f9bb9c2e17543
Dima Samoilov <dimasikman3@mail.ru>
2022-05-05 12:27:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab_08_notes_with_database/g.java
@@ -0,0 +1,5 @@
+package com.example.lab_08_notes_with_database;
+
+public final class g {
+    static DB notes;
+}
---
+++ app/src/main/java/com/example/lab_08_notes_with_database/mynote.java
@@ -0,0 +1,11 @@
+package com.example.lab_08_notes_with_database;
+
+public class mynote {
+
+    public int id;
+    public String txt;
+
+    public String toString() {
+        return String.valueOf(id) + " " + txt;
+    }
+}
